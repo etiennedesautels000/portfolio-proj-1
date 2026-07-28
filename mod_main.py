fb860f86adcc06668ce410d6db28db9dc5310fb6
Etienne Desautels <etienne.desautels@hotmail.ca>
2026-07-28 12:48:16 -0400

    Fix database init for Gunicorn

--- mod_main.py
+++ mod_main.py
@@ -5,8 +5,8 @@
 from app_calculer_moyenne.routes import app
 from app_calculer_moyenne.dao.mod_dao import init_db
 
+with app.app_context():  # initialisation de la bdd
+    init_db()
 if __name__ == "__main__":
-    with app.app_context(): # initialisation de la bdd
-        init_db()
     app.run(debug=True, port=5600) # lancer le serveur flask
 
